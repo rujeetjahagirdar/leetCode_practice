0de20328db816673fa0aaab5d23e7959dfea63fe
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2024-07-19 10:54:49 -0500

    Time: 575 ms (52.55%), Space: 79.4 MB (7.93%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,54 +1,97 @@
 class Node:
     def __init__(self, key=None, val=None):
-        self.key = key
-        self.val = val
         self.prev = None
-        self.next = None
-
+        self.next=None
+        self.val = val
+        self.key = key
+        
 class LRUCache:
+
     def __init__(self, capacity: int):
         self.cache = {}
         self.capacity = capacity
-        # Left sentinel node for least recently used
         self.left = Node()
-        # Right sentinel node for most recently used
         self.right = Node()
         self.left.next = self.right
         self.right.prev = self.left
         
+        
     def remove(self, node):
-        # Remove node from the linked list
-        prev, nxt = node.prev, node.next
-        prev.next = nxt
-        nxt.prev = prev
+        # print("remove")
+        node.prev.next = node.next
+        node.next.prev = node.prev
+        # prev, nxt = node.prev, node.next
+        # prev.next  = nxt
+        # nxt.prev = prev
+        # print(self.cache, self.left.next.val, self.right.prev.val)
     
     def insert(self, node):
-        # Insert node at the right (most recently used)
-        prev, nxt = self.right.prev, self.right
-        prev.next = node
-        node.prev = prev
-        node.next = nxt
-        nxt.prev = node
+        
+        self.right.prev.next = node
+        node.next = self.right
+        node.prev = self.right.prev
+        self.right.prev = node
+        
+        # prev, nxt = self.right.prev, self.right
+        # prev.next = node
+        # node.prev = prev
+        # node.next = nxt
+        # nxt.prev = node
+        # print(self.cache, self.left.next.val, self.right.prev.val)
+        
+        
+        
         
     def get(self, key: int) -> int:
+        # if key in cache:
+            #return the value
+            #remove that node
+            #insert that node
+        #else:
+            #return -1
         if key in self.cache:
-            # Move the accessed node to the most recently used position
+            # self.cache[key] = Node(key, self.cache[key].val)
             self.remove(self.cache[key])
             self.insert(self.cache[key])
             return self.cache[key].val
-        return -1
+        else:
+            return -1
 
     def put(self, key: int, value: int) -> None:
+        #if key in cache:
+            #remoove node
+            #insert new node
+        #else
+            #if capacity full:
+                #remove least used
+                #insert new node
+            #else:
+                #insert new node
         if key in self.cache:
             self.remove(self.cache[key])
-        self.cache[key] = Node(key, value)
-        self.insert(self.cache[key])
-        
-        if len(self.cache) > self.capacity:
-            # Remove the least recently used node
-            lru = self.left.next
-            self.remove(lru)
-            del self.cache[lru.key]
+            self.cache[key] = Node(key, value)
+            self.insert(self.cache[key])
+        else:
+            if len(self.cache)>=self.capacity  :
+                lru = self.left.next
+                self.remove(lru)
+                del self.cache[lru.key]
+                self.cache[key] = Node(key, value)
+                self.insert(self.cache[key])
+            else:
+                self.cache[key] = Node(key, value)
+                self.insert(self.cache[key])
+        # print(key, value)
+        #######
+        # if key in self.cache:
+        #     self.remove(self.cache[key])
+        # self.cache[key] = Node(key, value)
+        # # print(self.cache[key].val)
+        # self.insert(self.cache[key])
+        # if len(self.cache)>self.capacity:
+        #     lru = self.left.next
+        #     self.remove(lru)
+        #     del self.cache[lru.key]
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
