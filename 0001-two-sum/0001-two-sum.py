32da01bfff38efd960554b323f32c02e48d4d3ec
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2024-08-04 23:45:30 -0700

    Time: 59 ms (65.62%), Space: 17.7 MB (26.31%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,5 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+#         for i in range(len(nums)-1):
+#             for j in range(i+1,len(nums)):
+#                 if(nums[j]==(target-nums[i])):
+#                     return [i,j]
+        
+        ##########
         hashM = {}
         for i in range(len(nums)):
             compliment = target - nums[i]
