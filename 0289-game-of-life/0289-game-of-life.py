babf7fcd6e2e00dd2c195461fd0e6fa7ad471fcf
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2024-08-26 15:37:49 -0700

    0289-GAME-OF-LIFE Stats: Time: 48 ms (7.57%), Space: 16.7 MB (18.24%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.py
+++ 0289-game-of-life/0289-game-of-life.py
@@ -6,39 +6,46 @@ class Solution:
         m = len(board)
         n = len(board[0])
         nextState = {0: [], 1: []}
+        directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
         for i in range(m):
             for j in range(n):
                 liveN=0
-                if(0<=i-1<m):
-                    if(0<=j-1<n):
-                        if board[i-1][j-1] ==1:
+                # if(0<=i-1<m):
+                #     if(0<=j-1<n):
+                #         if board[i-1][j-1] ==1:
+                #             liveN +=1
+                #     if(0<=j<n):
+                #         if board[i-1][j] ==1:
+                #             liveN +=1
+                #     if(0<=j+1<n):
+                #         if board[i-1][j+1] ==1:
+                #             liveN +=1
+                # if(0<=i<m):
+                #     if(0<=j-1<n):
+                #         if board[i][j-1] ==1:
+                #             liveN +=1
+                #     # if(0<=j<n):
+                #     #     if board[i][j] ==1:
+                #     #         liveN +=1
+                #     if(0<=j+1<n):
+                #         if board[i][j+1] ==1:
+                #             liveN +=1
+                # if(0<=i+1<m):
+                #     if(0<=j-1<n):
+                #         if board[i+1][j-1] ==1:
+                #             liveN +=1
+                #     if(0<=j<n):
+                #         if board[i+1][j] ==1:
+                #             liveN +=1
+                #     if(0<=j+1<n):
+                #         if board[i+1][j+1] ==1:
+                #             liveN +=1
+                for d in directions:
+                    newi, newj = i+d[0], j+d[1]
+                    if(0<=newi<m and 0<=newj<n):
+                        if(board[newi][newj]==1):
                             liveN +=1
-                    if(0<=j<n):
-                        if board[i-1][j] ==1:
-                            liveN +=1
-                    if(0<=j+1<n):
-                        if board[i-1][j+1] ==1:
-                            liveN +=1
-                if(0<=i<m):
-                    if(0<=j-1<n):
-                        if board[i][j-1] ==1:
-                            liveN +=1
-                    # if(0<=j<n):
-                    #     if board[i][j] ==1:
-                    #         liveN +=1
-                    if(0<=j+1<n):
-                        if board[i][j+1] ==1:
-                            liveN +=1
-                if(0<=i+1<m):
-                    if(0<=j-1<n):
-                        if board[i+1][j-1] ==1:
-                            liveN +=1
-                    if(0<=j<n):
-                        if board[i+1][j] ==1:
-                            liveN +=1
-                    if(0<=j+1<n):
-                        if board[i+1][j+1] ==1:
-                            liveN +=1
+
                 print(i,j, liveN)
                 if(board[i][j]==1):
                     if(liveN<2):
