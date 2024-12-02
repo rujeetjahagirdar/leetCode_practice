354411fa821db73595f3b2b830f2534df265ef44
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2024-12-01 16:12:16 -0800

    1468-CHECK-IF-N-AND-ITS-DOUBLE-EXIST Stats: Time: 12 ms (18.81%), Space: 17.4 MB (6.92%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.py
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.py
@@ -1,7 +1,18 @@
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
-        arr=sorted(arr)
+        # arr=sorted(arr)
+        # for i in range(len(arr)):
+        #     if(arr[i]*2 in arr[i+1:] or arr[i]*2 in arr[:i]):
+        #         return True
+        # return False
+
+        hashSet = set()
         for i in range(len(arr)):
-            if(arr[i]*2 in arr[i+1:] or arr[i]*2 in arr[:i]):
+            if(arr[i]*2 in hashSet):
                 return True
+            elif(arr[i]%2==0 and arr[i]//2 in hashSet):
+                return True
+            else:
+                hashSet.add(arr[i])
+            print(hashSet)
         return False
