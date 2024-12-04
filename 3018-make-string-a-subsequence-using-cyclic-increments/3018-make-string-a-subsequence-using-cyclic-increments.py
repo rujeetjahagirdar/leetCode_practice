3f34cfaa0f7e902f33f83833e075082843045fde
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2024-12-03 20:28:12 -0800

    3018-MAKE-STRING-A-SUBSEQUENCE-USING-CYCLIC-INCREMENTS Stats: Time: 137 ms (11.81%), Space: 17.9 MB (5.69%) - LeetHub

--- 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.py
+++ 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.py
@@ -3,10 +3,21 @@ class Solution:
         i=0
         j=0
 
-        
+        def get_diff(s2, s1):
+            if(ord(s1)==ord(s2)):
+                return 0
+            # elif(((ord(s1)-ord('a'))+1)%26  ==(ord(s2)-ord('a'))):
+            #     return 1
+            else:
+                s2_index = ord(s2) - ord('a')
+                s1_index = ord(s1) - ord('a')
+                if((s1_index+1)%26 == s2_index):
+                    return 1
+
 
         while(i<len(str2)):
-            while(j<len(str1) and (ord(str2[i]) - ord(str1[j]) + 26) % 26 not in (0,1)):
+            # while(j<len(str1) and (ord(str2[i]) - ord(str1[j]) + 26) % 26 not in (0,1)):
+            while(j<len(str1) and get_diff(str2[i], str1[j]) not in (0,1)):
                 j+=1
             
             if(j>=len(str1)):
