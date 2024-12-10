2645c200ed27c73181285e2e1d58f969cb1f2a09
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2024-12-09 18:06:12 -0800

    3267-FIND-LONGEST-SPECIAL-SUBSTRING-THAT-OCCURS-THRICE-I Stats: Time: 21 ms (44.32%), Space: 17.3 MB (9.27%) - LeetHub

--- 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.py
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.py
@@ -1,25 +1,33 @@
+
+
+
 class Solution:
     def maximumLength(self, s: str) -> int:
-        ans = float('-inf')
+        # Create a dictionary to store the count of all substrings.
+        count = {}
+        count_strings = 0
+        for start in range(len(s)):
+            character = s[start]
+            substring_length = 0
+            for end in range(start, len(s)):
+                # If the string is empty, or the current character is equal to
+                # the previously added character, then add it to the map.
+                # Otherwise, break the iteration.
+                if character == s[end]:
+                    substring_length += 1
+                    count[(character, substring_length)] = (
+                        count.get((character, substring_length), 0) + 1
+                    )
+                else:
+                    break
 
-        def checkSpecial(substr):
-            return (substr[0]*len(substr) ==substr)
-        
-        for j in range(len(s)):
-            for i in range(1, len(s)):
-                subStr = s[j:j+i]
-                # print(subStr)
-                if(checkSpecial(subStr)):
-                    cnt = 1
-                    for k in range(j+1, len(s)):
-                        if(s[k:k+len(subStr)]==subStr):
-                            cnt+=1
-                            
-                    # print(subStr, cnt)
-                    if(cnt>=3):
-                        ans=max(ans, len(subStr))
-        print(ans)
-        if(ans==float('-inf')):
+        # Create a variable ans to store the longest length of substring with
+        # frequency atleast 3.
+        ans = 0
+        for i in count.items():
+            length = i[0][1]
+            if i[1] >= 3 and length > ans:
+                ans = length
+        if ans == 0:
             return -1
-        else:
-            return ans
+        return ans
