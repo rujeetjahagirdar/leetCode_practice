0b933ba3803b2af179dc79ae023bd99cde2aca75
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2024-12-25 12:37:06 -0800

    0975-RANGE-SUM-OF-BST Stats: Time: 31 ms (6.9%), Space: 25.3 MB (7.17%) - LeetHub

--- 0975-range-sum-of-bst/0975-range-sum-of-bst.py
+++ 0975-range-sum-of-bst/0975-range-sum-of-bst.py
@@ -6,25 +6,42 @@
 #         self.right = right
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
-        ans = 0
 
         if not root:
             return
 
-        q = deque([root])
+        # q = deque([root])
 
-        while(q):
+        # while(q):
 
-            for _ in range(len(q)):
-                n = q.popleft()
-                if(n.val>=low and n.val<=high):
-                    ans+=n.val
+        #     for _ in range(len(q)):
+        #         n = q.popleft()
+        #         if(n.val>=low and n.val<=high):
+        #             ans+=n.val
                 
-                if n.left:
-                    q.append(n.left)
+        #         if n.left:
+        #             q.append(n.left)
                 
-                if n.right:
-                    q.append(n.right)
+        #         if n.right:
+        #             q.append(n.right)
                 
-            print(ans)
-        return ans
+        #     print(ans)
+        # return ans
+        #####################
+
+
+        def inorderTraverse(node, ans):
+            if not node:
+                return
+            
+            inorderTraverse(node.left, ans)
+            if(node.val>=low and node.val<=high):
+                ans[0]+=node.val
+                print(ans)
+            inorderTraverse(node.right, ans)
+
+            return
+        
+        ans=[0]
+        inorderTraverse(root, ans)
+        return ans[0]
