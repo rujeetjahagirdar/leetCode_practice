9f7466dd9720af197f24d3552415acb230ccd16f
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-01-13 16:58:45 -0800

    2766-FIND-THE-PREFIX-COMMON-ARRAY-OF-TWO-ARRAYS Stats: Time: 20 ms (36.2%), Space: 17.8 MB (32.03%) - LeetHub

--- 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.py
+++ 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.py
@@ -1,7 +1,7 @@
 class Solution:
     def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
-        freqA = defaultdict(int)
-        freqB = defaultdict(int)
+        freqA = set()
+        freqB = set()
         ans=[0]*(len(A)+1)
 
         for i in range(len(A)):
@@ -13,8 +13,8 @@ class Solution:
             if(A[i] in freqB):
                 t+=1
             ans[i+1] = ans[i]+t
-            freqB[B[i]]+=1
-            freqA[A[i]]+=1
+            freqB.add(B[i])
+            freqA.add(A[i])
         print(freqA)
         print(freqB)
         print(ans)
