97dfe5e1dbc98f64126ca52cfe9e6970b0182eaf
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-01-16 16:32:56 -0800

    2792-NEIGHBORING-BITWISE-XOR Stats: Time: 42 ms (67.15%), Space: 22.4 MB (48.9%) - LeetHub

--- 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.py
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.py
@@ -5,32 +5,40 @@ class Solution:
         #[1,1,1,0]
         #[0,1,0,1]
 
-        original=[-1]*len(derived)
+        # original=[-1]*len(derived)
 
-        for i in range(len(derived)):
-            if(i==len(derived)-1):
-                if(derived[i]==0):
-                    if(original[i]==original[0]):
-                        return True
-                    else:
-                        return False
-                else:
-                    if(original[i]!=original[0]):
-                        return True
-                    else:
-                        return False
-            elif(i==0):
-                if(derived[i]==0):
-                    original[i]=0
-                    original[i+1]=0
-                else:
-                    original[i]=0
-                    original[i+1]=1
-            else:
-                if(derived[i]==0):
-                    # original[i]=0
-                    original[i+1]=original[i]
-                else:
-                    # original[i]=0
-                    original[i+1]=int(not original[i])
-            # print(original)
+        # for i in range(len(derived)):
+        #     if(i==len(derived)-1):
+        #         if(derived[i]==0):
+        #             if(original[i]==original[0]):
+        #                 return True
+        #             else:
+        #                 return False
+        #         else:
+        #             if(original[i]!=original[0]):
+        #                 return True
+        #             else:
+        #                 return False
+        #     elif(i==0):
+        #         if(derived[i]==0):
+        #             original[i]=0
+        #             original[i+1]=0
+        #         else:
+        #             original[i]=0
+        #             original[i+1]=1
+        #     else:
+        #         if(derived[i]==0):
+        #             # original[i]=0
+        #             original[i+1]=original[i]
+        #         else:
+        #             # original[i]=0
+        #             original[i+1]=int(not original[i])
+        #     # print(original)
+        ###########################
+        #2
+
+        ans=0
+        for i in derived:
+            ans^=i
+        
+        return ans==0
