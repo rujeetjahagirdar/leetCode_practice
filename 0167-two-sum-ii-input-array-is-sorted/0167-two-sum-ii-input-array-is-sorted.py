8ae2a54375b183311f2f687f12ac96071b943bde
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-01-25 15:18:49 -0800

    0167-TWO-SUM-II-INPUT-ARRAY-IS-SORTED Stats: Time: 1 ms (89.09%), Space: 18.6 MB (41.86%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,23 +1,36 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         
-        def bsearch(n, l, r):
-            while(l<=r):
-                mid = l+((r-l)//2)
-                if(numbers[mid]==n):
-                    return mid
-                elif(numbers[mid]<n):
-                    l=mid+1
-                else:
-                    r=mid-1
-            return None
+        # def bsearch(n, l, r):
+        #     while(l<=r):
+        #         mid = l+((r-l)//2)
+        #         if(numbers[mid]==n):
+        #             return mid
+        #         elif(numbers[mid]<n):
+        #             l=mid+1
+        #         else:
+        #             r=mid-1
+        #     return None
             
 
-        for i in range(len(numbers)):
-            compliment = target - numbers[i]
-            compli_index = bsearch(compliment, i+1, len(numbers)-1)
-            print(numbers[i])
-            print(compli_index)
-            # print(numbers[compli_index])
-            if(compli_index):
-                return [i+1, compli_index+1]
+        # for i in range(len(numbers)):
+        #     compliment = target - numbers[i]
+        #     compli_index = bsearch(compliment, i+1, len(numbers)-1)
+            
+        #     if(compli_index):
+        #         return [i+1, compli_index+1]
+
+
+        l=0
+        r = len(numbers)-1
+
+        while(l<=r):
+            s = numbers[l]+numbers[r]
+
+            if(s==target):
+                return [l+1, r+1]
+            elif(s<target):
+                l+=1
+            else:
+                r-=1
+        
