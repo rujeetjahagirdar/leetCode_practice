0b99344fd6ce456cb9985dc38d62748f3a3ae0cc
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-02-13 16:41:00 -0800

    1477-PRODUCT-OF-THE-LAST-K-NUMBERS Stats: Time: 47 ms (32.88%), Space: 31.3 MB (95.89%) - LeetHub

--- 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.py
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.py
@@ -1,30 +1,58 @@
+# class ProductOfNumbers:
+
+#     def __init__(self):
+#         self.stream = []
+#         self.totalProduct = 1
+#         self.prefixProduct = [1]
+
+#     def add(self, num: int) -> None:
+        
+#         self.stream.append(num)
+#         if(num==0):
+#             self.totalProduct*=1
+#             self.prefixProduct.append(self.prefixProduct[-1]*1)
+#         else:
+#             self.totalProduct*=num
+#             self.prefixProduct.append(self.prefixProduct[-1]*num)
+#         # print(self.stream)
+#         # print(self.totalProduct)
+#         # print(self.prefixProduct)
+
+#     def getProduct(self, k: int) -> int:
+#         if(0 in self.stream[-k:]):
+#             return 0
+#         else:
+#             idx = (len(self.stream) - k)
+#             return(self.totalProduct//self.prefixProduct[idx])
+        
+
+
+# # Your ProductOfNumbers object will be instantiated and called as such:
+# # obj = ProductOfNumbers()
+# # obj.add(num)
+# # param_2 = obj.getProduct(k)
+##############################
 class ProductOfNumbers:
 
     def __init__(self):
-        self.stream = []
-        self.totalProduct = 1
-        self.prefixProduct = [1]
+        self.prefixProduct = []
+        self.runningProduct = 1
 
     def add(self, num: int) -> None:
-        
-        self.stream.append(num)
         if(num==0):
-            self.totalProduct*=1
-            self.prefixProduct.append(self.prefixProduct[-1]*1)
+            self.prefixProduct = []
+            self.runningProduct = 1 
         else:
-            self.totalProduct*=num
-            self.prefixProduct.append(self.prefixProduct[-1]*num)
-        # print(self.stream)
-        # print(self.totalProduct)
-        # print(self.prefixProduct)
+            self.runningProduct *=num
+            self.prefixProduct.append(self.runningProduct)
 
     def getProduct(self, k: int) -> int:
-        if(0 in self.stream[-k:]):
+        if(k>len(self.prefixProduct)):
             return 0
+        elif(k==len(self.prefixProduct)):
+            return self.prefixProduct[-1]
         else:
-            idx = (len(self.stream) - k)
-            return(self.totalProduct//self.prefixProduct[idx])
-        
+            return self.prefixProduct[-1]//self.prefixProduct[-(k+1)]
 
 
 # Your ProductOfNumbers object will be instantiated and called as such:
