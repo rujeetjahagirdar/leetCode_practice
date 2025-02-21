69ce86801c9fcb62b692701e012e2950f8d6868f
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-02-20 17:52:01 -0800

    1387-FIND-ELEMENTS-IN-A-CONTAMINATED-BINARY-TREE Stats: Time: 19 ms (19.25%), Space: 22.4 MB (10.7%) - LeetHub

--- 1387-find-elements-in-a-contaminated-binary-tree/1387-find-elements-in-a-contaminated-binary-tree.py
+++ 1387-find-elements-in-a-contaminated-binary-tree/1387-find-elements-in-a-contaminated-binary-tree.py
@@ -7,37 +7,28 @@
 class FindElements:
 
     def __init__(self, root: Optional[TreeNode]):
-        self.treeLevels = []
+        # self.treeLevels = []
         self.q = deque([root])
         self.elements = set()
         root.val = 0
         while(self.q):
-            t = []
+            # t = []
             for i in range(len(self.q)):
                 n = self.q.popleft()
 
-                t.append(n.val)
+                # t.append(n.val)
                 self.elements.add(n.val)
                 if(n.left and n.left.val==-1):
-                    # print("Left")
                     v = (2*n.val)+1
                     n.left.val = v
-                    # print("v",v)
                     self.q.append(n.left)
                 
                 if(n.right and n.right.val==-1):
                     v = (2*n.val)+2
                     n.right.val = v
                     self.q.append(n.right)
-                # if(not n.left and not n.right):
-                #     continue
-                # if(not n.left and n.right):
-                #     self.q.append(TreeNode(-2))
-                # if(not n.right and n.left):
-                #     self.q.append(TreeNode(-2))
-                # # print("t= ", t)
-            self.treeLevels.append(t)
-            print(self.treeLevels)
+            # self.treeLevels.append(t)
+            # print(self.treeLevels)
             print(self.elements)
 
 
