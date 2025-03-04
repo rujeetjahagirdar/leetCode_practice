699eb180fd7876f5c73a42fdb8b84323868a9d6e
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-03-03 18:49:19 -0800

    1909-BUILDINGS-WITH-AN-OCEAN-VIEW Stats: Time: 32 ms (47.72%), Space: 31.2 MB (24.62%) - LeetHub

--- 1909-buildings-with-an-ocean-view/1909-buildings-with-an-ocean-view.py
+++ 1909-buildings-with-an-ocean-view/1909-buildings-with-an-ocean-view.py
@@ -1,19 +1,31 @@
 class Solution:
     def findBuildings(self, heights: List[int]) -> List[int]:
-        prefixArr = [0]*(len(heights)+1)
-        heights = heights[::-1]
-        mxHeight = heights[0]
-        for i in range(1,len(heights)):
-            prefixArr[i+1] = mxHeight
-            mxHeight = max(mxHeight, heights[i])
-        heights=heights[::-1]
-        prefixArr=prefixArr[::-1]
-        print(heights[::-1])
-        print(prefixArr[::-1])
+        # prefixArr = [0]*(len(heights)+1)
+        # heights = heights[::-1]
+        # mxHeight = heights[0]
+        # for i in range(1,len(heights)):
+        #     prefixArr[i+1] = mxHeight
+        #     mxHeight = max(mxHeight, heights[i])
+        # heights=heights[::-1]
+        # prefixArr=prefixArr[::-1]
+        # print(heights[::-1])
+        # print(prefixArr[::-1])
+        # ans=[]
+
+        # for i in range(len(heights)):
+        #     if(heights[i]>prefixArr[i]):
+        #         ans.append(i)
+        # print(ans)
+        # return ans
+        ########################
         ans=[]
 
-        for i in range(len(heights)):
-            if(heights[i]>prefixArr[i]):
+        mxHeight = 0
+
+        for i in reversed(range(len(heights))):
+            # print(heights[i])
+            if(heights[i]>mxHeight):
                 ans.append(i)
-        print(ans)
-        return ans
+                mxHeight = max(mxHeight, heights[i])
+        print(ans[::-1])
+        return ans[::-1]
