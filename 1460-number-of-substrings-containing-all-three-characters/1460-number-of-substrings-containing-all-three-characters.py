b52ecab0d5068cacab56567097fec58d2c3beb3d
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-03-10 19:31:22 -0700

    1460-NUMBER-OF-SUBSTRINGS-CONTAINING-ALL-THREE-CHARACTERS Stats: Time: 80 ms (78.1%), Space: 17.8 MB (90.82%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.py
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.py
@@ -1,26 +1,37 @@
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
+        # ans=0
+
+        # l=0
+        # counts = defaultdict(int)
+        
+        # for r in range(len(s)):
+        #     counts[s[r]]+=1
+        #     # print(counts)
+        #     if(len(counts)==3):
+        #         # ans+=1
+        #         # ans+=(len(s)-(r))
+        #         # print(ans)
+
+        #         while(len(counts)==3):
+        #             # print(s[l])
+        #             ans+=(len(s)-(r))
+        #             counts[s[l]]-=1
+        #             if(counts[s[l]]==0):
+        #                 counts.pop(s[l])
+        #             # print(counts)
+        #             # ans+=1
+        #             l+=1
+        #     print(ans)
+        # return ans
+        ####################
         ans=0
 
-        l=0
-        counts = defaultdict(int)
+        latest_idx = {'a':-1, 'b':-1, 'c':-1}
         
-        for r in range(len(s)):
-            counts[s[r]]+=1
-            # print(counts)
-            if(len(counts)==3):
-                # ans+=1
-                # ans+=(len(s)-(r))
-                # print(ans)
+        for i in range(len(s)):
+            latest_idx[s[i]]=i
 
-                while(len(counts)==3):
-                    # print(s[l])
-                    ans+=(len(s)-(r))
-                    counts[s[l]]-=1
-                    if(counts[s[l]]==0):
-                        counts.pop(s[l])
-                    # print(counts)
-                    # ans+=1
-                    l+=1
-            print(ans)
+            if(latest_idx['a']!=-1 and latest_idx['b']!=-1 and latest_idx['c']!=-1):
+                ans+=(1+ min(latest_idx['a'], latest_idx['b'], latest_idx['c']))
         return ans
