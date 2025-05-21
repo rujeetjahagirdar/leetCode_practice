904e8a3712867a7cc9f9803a636915b0a02a6931
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-05-20 19:27:31 -0700

    1036-ROTTING-ORANGES Stats: Time: 0 ms (100%), Space: 17.6 MB (93.17%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -5,21 +5,6 @@ class Solution:
         q = deque()
         max_time=0
         fresh_count=0
-
-
-        def bfs():
-            nonlocal fresh_count
-            nonlocal max_time
-            while(q and fresh_count>0):
-                i, j, d = q.popleft()
-
-                for direction in directions:
-                    new_i, new_j = direction[0]+i, direction[1]+j
-                    if(0<=new_i<len(grid) and 0<=new_j<len(grid[0]) and grid[new_i][new_j]==1):
-                        grid[new_i][new_j] = 2
-                        fresh_count-=1
-                        q.append((new_i, new_j, d+1))
-                        max_time = max(max_time, d+1)
         
 
         for i in range(len(grid)):
@@ -28,7 +13,18 @@ class Solution:
                     q.append((i, j, 0))
                 elif(grid[i][j]==1):
                     fresh_count +=1
-        bfs()
+        
+        while(q and fresh_count>0):
+            i, j, d = q.popleft()
+
+            for direction in directions:
+                new_i, new_j = direction[0]+i, direction[1]+j
+                if(0<=new_i<len(grid) and 0<=new_j<len(grid[0]) and grid[new_i][new_j]==1):
+                    grid[new_i][new_j] = 2
+                    fresh_count-=1
+                    q.append((new_i, new_j, d+1))
+                    max_time = max(max_time, d+1)
+
         print(max_time)
         if(fresh_count==0):
             return max_time
