290a931ff658a84332a75e6cc0b2e79086d10c70
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-06-19 00:02:42 -0700

    1708-DESIGN-PARKING-SYSTEM Stats: Time: 4 ms (96.28%), Space: 18.2 MB (95.01%) - LeetHub

--- 1708-design-parking-system/1708-design-parking-system.py
+++ 1708-design-parking-system/1708-design-parking-system.py
@@ -1,30 +1,16 @@
 class ParkingSystem:
 
     def __init__(self, big: int, medium: int, small: int):
-        self.big = big
-        self.medium = medium
-        self.small = small
+        self.space = {1: big, 2: medium, 3: small}
         self.parking = {1: 0, 2: 0, 3: 0}
 
     def addCar(self, carType: int) -> bool:
-        if(carType==1):
-            if(self.parking[carType]<self.big):
-                self.parking[carType]+=1
-                return True
-            else:
-                return False
-        if(carType==2):
-            if(self.parking[carType]<self.medium):
-                self.parking[carType]+=1
-                return True
-            else:
-                return False
-        if(carType==3):
-            if(self.parking[carType]<self.small):
-                self.parking[carType]+=1
-                return True
-            else:
-                return False
+
+        if(self.parking[carType]<self.space[carType]):
+            self.parking[carType]+=1
+            return True
+        else:
+            return False
 
 
 # Your ParkingSystem object will be instantiated and called as such:
