a308558bb75ee28175caf9add8bed600b3fd4e32
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-07-29 18:01:14 -0700

    0235-LOWEST-COMMON-ANCESTOR-OF-A-BINARY-SEARCH-TREE Stats: Time: 1341 ms (5.75%), Space: 25.3 MB (5.6%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,19 +7,55 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        p_val = p.val
-        q_val = q.val
+        # p_val = p.val
+        # q_val = q.val
 
-        node = root
+        # node = root
 
-        while node:
-            parent_val = node.val
+        # while node:
+        #     parent_val = node.val
 
-            if(p_val> parent_val and q_val>parent_val):
-                node = node.right
+        #     if(p_val> parent_val and q_val>parent_val):
+        #         node = node.right
             
-            elif(p_val< parent_val and q_val<parent_val):
-                node = node.left
+        #     elif(p_val< parent_val and q_val<parent_val):
+        #         node = node.left
             
-            else:
-                return node
+        #     else:
+        #         return node
+        #################
+        p_path = []
+        q_path = []
+
+        def dfs(node, target):
+
+            if not node:
+                return []
+
+            stack = [(node, [node])]
+
+            while stack:
+                n, path = stack.pop()
+                # print(n)
+                if(n==target):
+                    return path
+                if(n.right):
+                    stack.append((n.right, path+[n.right]))
+                if(n.left):
+                    stack.append((n.left, path+[n.left]))
+            return []
+        
+        p_path = dfs(root, p)
+        q_path = dfs(root, q)
+
+        print(p_path)
+        print(q_path)
+
+        i=0
+        ans=-1
+        while(i<len(p_path) and i<len(q_path)):
+            if(p_path[i] in q_path):
+                ans = p_path[i]
+            i+=1
+        
+        return(ans)
