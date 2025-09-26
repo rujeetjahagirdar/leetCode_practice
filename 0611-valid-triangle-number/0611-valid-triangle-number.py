b9e9880c75c2a213dd2034b1daa077ce4fea17db
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-09-25 18:40:38 -0700

    0611-VALID-TRIANGLE-NUMBER Stats: Time: 389 ms (98.71%), Space: 18 MB (34.35%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.py
+++ 0611-valid-triangle-number/0611-valid-triangle-number.py
@@ -1,38 +1,55 @@
 class Solution:
     def triangleNumber(self, nums: List[int]) -> int:
         
-        def  bsearch(a, t):
-            l=0
-            r = len(a)-1
-            ans = len(a)
+        # def  bsearch(a, t):
+        #     l=0
+        #     r = len(a)-1
+        #     ans = len(a)
 
-            while(l<=r):
-                mid = (l+r)//2
+        #     while(l<=r):
+        #         mid = (l+r)//2
 
-                if(a[mid]>=t):
-                    ans=mid
-                    r=mid-1
-                else:
-                    l=mid+1
+        #         if(a[mid]>=t):
+        #             ans=mid
+        #             r=mid-1
+        #         else:
+        #             l=mid+1
             
-            return ans
+        #     return ans
      
+
+        # ans=0
+        # nums.sort()
+        # #[2,2,3,4]
+        # for i in range(len(nums)-2):
+        #     for j in range(i+1, len(nums)-1):
+        #         k = j+1+bsearch(nums[j+1:], nums[i]+nums[j])
+        #         # if(k!=-1):
+        #         # print(k)
+        #         # print(nums[i], nums[j], nums[k-1])
+        #         if((nums[i]+nums[j])<=nums[k-1]):
+        #             continue
+        #         ans+=(k-1 - j)
+        #         # print(ans)
+        # return ans
+        
+
 
         ans=0
         nums.sort()
         #[2,2,3,4]
-        for i in range(len(nums)-2):
-            for j in range(i+1, len(nums)-1):
-                k = j+1+bsearch(nums[j+1:], nums[i]+nums[j])
-                # if(k!=-1):
-                # print(k)
-                # print(nums[i], nums[j], nums[k-1])
-                if((nums[i]+nums[j])<=nums[k-1]):
-                    continue
-                ans+=(k-1 - j)
-                # print(ans)
+
+        for k in range(len(nums)-1, 1, -1):
+            c = nums[k]
+
+            l=0
+            r = k-1
+
+            while(l<r):
+                if(nums[l]+nums[r]<=c):
+                    l+=1
+                else:
+                    ans+=(r-l)
+                    r-=1
+            # print(ans)
         return ans
-        
-
-
-                
