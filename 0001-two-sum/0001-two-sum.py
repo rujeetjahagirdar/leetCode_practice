01f53ead69531fe9c6dbf63446e80acb1a855c98
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-10-05 13:56:10 -0700

    0001-TWO-SUM Stats: Time: 2 ms (52.66%), Space: 18.8 MB (62.38%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_map = {}
-        for index, num in enumerate(nums):
-            complement = target - num
-            if complement in num_map:
-                return [num_map[complement], index]
-            num_map[num] = index
+        hashM = {}
 
-# Runtime Complexity: O(n)
-# Space Complexity: O(n)
+        for i in range(len(nums)):
+            if(target- nums[i] in hashM):
+                return [hashM[target-nums[i]], i]
+            else:
+                hashM[nums[i]]=i
