69ffd64e3b1d0aac24e4c39f39df41925fc9c881
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-10-05 14:02:37 -0700

    0020-VALID-PARENTHESES Stats: Time: 0 ms (100%), Space: 18.1 MB (14.08%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,42 +1,20 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        # hashM = {'(':0, '{':0, '[':0}
-        # for i in s:
-        #     if(i=='('):
-        #         hashM[i] = hashM[i]+1
-        #     elif(i=='{'):
-        #         hashM[i] = hashM[i]+1
-        #     elif(i=='['):
-        #         hashM[i] = hashM[i]+1
-        #     elif(i==')'):
-        #         if(hashM['(']==0):
-        #             return False
-        #         hashM['('] = hashM['(']-1
-        #     elif(i==']'):
-        #         if(hashM['[']==0):
-        #             return False
-        #         hashM['['] = hashM['[']-1
-        #     elif(i=='}'):
-        #         if(hashM['{']==0):
-        #             return False
-        #         hashM['{'] = hashM['{']-1
-        # for n in list(hashM.values()):
-        #     if(n!=0):
-        #         return False
-        # return True
-
-        #stack
         stack=[]
-        parentheses_pairs = {')':'(', ']':'[', '}':'{'}
+
         for i in s:
-            if(i in parentheses_pairs):
-                if(stack and stack[-1]==parentheses_pairs[i]):
-                    stack.pop()
+            if(i in ('(', '{', '[')):
+                stack.append(i)
+            else:
+                if(len(stack)>0):
+                    if(i==')' and stack[-1]=='('):
+                        stack.pop()
+                    elif(i=='}' and stack[-1]=='{'):
+                        stack.pop()
+                    elif(i==']' and stack[-1]=='['):
+                        stack.pop()
+                    else:
+                        return False
                 else:
                     return False
-            else:
-                stack.append(i)
-        if(stack):
-            return False
-        else:
-            return True
+        return len(stack)==0
