c067ab7c4986ec83bfcfd7c47d83be2df0402b58
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-10-05 13:03:21 -0700

    0680-VALID-PALINDROME-II Stats: Time: 30 ms (92.21%), Space: 18 MB (35.26%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,22 +1,23 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        #abbxa
-        def checkPalin(substr):
-            return substr==substr[::-1]
+        
+        def checkPalim(s):
+            if(len(s)==1):
+                return True
+            return s[:]==s[::-1]
 
-        i=0
-        j=len(s)-1
+        l=0
+        r=len(s)-1
 
-        while(i<j):
-            if(s[i]==s[j]):
-                i+=1
-                j-=1
-            else:
-                # if(checkPalin(s[i:j])):
-                #     return True
-                # elif(checkPalin(s[i+1:j+1])):
-                #     return True
-                # else:
-                #     return False
-                return checkPalin(s[i:j]) or checkPalin(s[i+1:j+1])
+
+        while(l<=r):
+            if(s[l]==s[r]):
+                l+=1
+                r-=1
+            elif(s[l]!=s[r]):
+                if(checkPalim(s[l:r])):
+                    return True
+                if(checkPalim(s[l+1:r+1])):
+                    return True
+                return False
         return True
