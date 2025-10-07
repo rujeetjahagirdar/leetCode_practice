905ada05054cce04ab5b249c015677f7bb3501a1
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-10-07 00:39:06 -0700

    0003-LONGEST-SUBSTRING-WITHOUT-REPEATING-CHARACTERS Stats: Time: 38 ms (15.67%), Space: 18.4 MB (12.27%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -3,23 +3,17 @@ class Solution:
         ans=0
 
         l=0
-        r=0
+        window = set()
 
-        sliding_window_set = set()
-
-        while(r<len(s)):
-            if(s[r] not in sliding_window_set):
-                sliding_window_set.add(s[r])
-                # print(sliding_window_set)
-                ans = max(ans, len(sliding_window_set))
+        for r in range(len(s)):
+            if(s[r] not in window):
+                window.add(s[r])
+                ans=max(ans, len(window))
             else:
-                while(s[l]!=s[r]):
-                    sliding_window_set.remove(s[l])
+                while(s[r] in window):
+                    window.remove(s[l])
                     l+=1
-                sliding_window_set.remove(s[l])
-                # print(sliding_window_set)
-                l+=1
-                sliding_window_set.add(s[r])
-            r+=1
-        print(ans)
+                window.add(s[r])
+                ans=max(ans, len(window))
+            print(ans)
         return ans
