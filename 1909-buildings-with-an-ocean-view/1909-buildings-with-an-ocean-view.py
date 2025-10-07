9d179a57cbda3cb4801d39fecf859940bc497c0c
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-10-07 15:03:49 -0700

    1909-BUILDINGS-WITH-AN-OCEAN-VIEW Stats: Time: 30 ms (51.34%), Space: 31 MB (48.77%) - LeetHub

--- 1909-buildings-with-an-ocean-view/1909-buildings-with-an-ocean-view.py
+++ 1909-buildings-with-an-ocean-view/1909-buildings-with-an-ocean-view.py
@@ -1,31 +1,10 @@
 class Solution:
     def findBuildings(self, heights: List[int]) -> List[int]:
-        # prefixArr = [0]*(len(heights)+1)
-        # heights = heights[::-1]
-        # mxHeight = heights[0]
-        # for i in range(1,len(heights)):
-        #     prefixArr[i+1] = mxHeight
-        #     mxHeight = max(mxHeight, heights[i])
-        # heights=heights[::-1]
-        # prefixArr=prefixArr[::-1]
-        # print(heights[::-1])
-        # print(prefixArr[::-1])
-        # ans=[]
+        maxTill = heights[-1]
+        ans = [len(heights)-1]
 
-        # for i in range(len(heights)):
-        #     if(heights[i]>prefixArr[i]):
-        #         ans.append(i)
-        # print(ans)
-        # return ans
-        ########################
-        ans=[]
-
-        mxHeight = 0
-
-        for i in reversed(range(len(heights))):
-            # print(heights[i])
-            if(heights[i]>mxHeight):
+        for i in range(len(heights)-2, -1, -1):
+            if(heights[i]>maxTill):
                 ans.append(i)
-                mxHeight = max(mxHeight, heights[i])
-        print(ans[::-1])
-        return ans[::-1]
+                maxTill = max(maxTill, heights[i])
+        return(ans[::-1])
