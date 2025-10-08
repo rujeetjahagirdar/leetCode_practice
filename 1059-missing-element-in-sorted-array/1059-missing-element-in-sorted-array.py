ed865c9810e71bd9599cfbaf82b2dd05ca862e89
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-10-08 16:45:24 -0700

    1059-MISSING-ELEMENT-IN-SORTED-ARRAY Stats: Time: 12 ms (10.53%), Space: 22.5 MB (88.1%) - LeetHub

--- 1059-missing-element-in-sorted-array/1059-missing-element-in-sorted-array.py
+++ 1059-missing-element-in-sorted-array/1059-missing-element-in-sorted-array.py
@@ -7,3 +7,19 @@ class Solution:
             else:
                 k-=((nums[i+1]-nums[i])-1)
         return nums[-1]+k
+
+
+
+        l=0
+        r = len(nums)-1
+
+        while(l<=r):
+            mid = l + (r-l)//2
+
+            noOfMissings = (nums[mid] - nums[0])-mid
+
+            if(noOfMissings<k):
+                return nums[mid]+(k-noOfMissings)
+            else:
+                r = mid
+        return nums[-1] + (k - (nums[-1]-nums[0])-len(nums)-1)
