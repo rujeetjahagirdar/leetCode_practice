359242de7fa7977d46e6ace137336ee43a9ce87b
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-10-08 16:18:44 -0700

    1320-REMOVE-ALL-ADJACENT-DUPLICATES-IN-STRING-II Stats: Time: 54 ms (24.33%), Space: 21.8 MB (77.06%) - LeetHub

--- 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.py
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.py
@@ -1,20 +1,26 @@
 class Solution:
     def removeDuplicates(self, s: str, k: int) -> str:
-        stack = [] #(c, count)
+        
+        #(lastChar, freq of lastChar)
+        stack = []
+
+        
 
         for i in range(len(s)):
-            if(i==0 or len(stack)==0):
-                stack.append((s[i], 1))
-            else:
-                # print(s[i], stack[-1][1])
-                if(s[i]==stack[-1][0]):
-                    c, cnt = stack.pop()
-                    stack.append((c, cnt+1))
 
-                    if(stack[-1][1]==k):
-                        stack.pop()
-                else:
-                    stack.append((s[i], 1))
+            if(stack and s[i]==stack[-1][0]):
+                char, freq = stack.pop()
+                stack.append((char, freq+1))
+            else:
+                stack.append((s[i], 1))
+            
+            if(stack[-1][1]==k):
+                stack.pop()
             # print(stack)
         
-        return ''.join(i[0]*i[1] for i in stack)
+        ans=''
+
+        for i in stack:
+            ans+=i[0]*i[1]
+        # print(ans)
+        return ans
