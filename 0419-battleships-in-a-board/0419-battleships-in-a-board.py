657320c994e486ea0a82f38d764677c2ae2424e4
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-10-09 14:33:01 -0700

    0419-BATTLESHIPS-IN-A-BOARD Stats: Time: 0 ms (100%), Space: 20.1 MB (65.91%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.py
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.py
@@ -16,14 +16,23 @@ class Solution:
         for i in range(len(board)):
             for j in range(len(board[0])):
                 if(board[i][j]=='X'):
-                    if(i==0):
-                        if(j>0 and board[i][j-1]!='X'):
-                            ans+=1
-                        elif(j==0):
-                            ans+=1
-                    elif(j==0):
-                        if(i>0 and board[i-1][j]!='X'):
-                            ans+=1
+                    # if(i==0):
+                    #     if(j>0 and board[i][j-1]!='X'):
+                    #         ans+=1
+                    #     elif(j==0):
+                    #         ans+=1
+                    # elif(j==0):
+                    #     if(i>0 and board[i-1][j]!='X'):
+                    #         ans+=1
+                    if(i==0 and j==0):
+                        ans+=1
+                        continue
+                    if(i==0 and board[i][j-1]!='X'):
+                        ans+=1
+                        continue
+                    if(j==0 and board[i-1][j]!='X'):
+                        ans+=1
+                        continue
                     if(i>0 and j>0):
                         if(board[i-1][j]!='X' and board[i][j-1]!='X'):
                             ans+=1
