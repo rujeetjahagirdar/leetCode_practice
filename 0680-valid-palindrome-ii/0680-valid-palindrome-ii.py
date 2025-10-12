5efc38345a0a65c1e4bc4b37dd81fdf6fd2ddb29
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-10-12 15:48:17 -0700

    0680-VALID-PALINDROME-II Stats: Time: 32 ms (86.67%), Space: 18.3 MB (9.63%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,20 +1,19 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
         
-        def checkPalim(s):
-            if(len(s)==1):
-                return True
-            return s[:]==s[::-1]
+        def checkPalim(t):
+            return t==t[::-1]
+
+        # k=1
 
         l=0
-        r=len(s)-1
+        r = len(s)-1
 
-
-        while(l<=r):
+        while(l<r):
             if(s[l]==s[r]):
                 l+=1
                 r-=1
-            elif(s[l]!=s[r]):
+            else:
                 if(checkPalim(s[l:r])):
                     return True
                 if(checkPalim(s[l+1:r+1])):
