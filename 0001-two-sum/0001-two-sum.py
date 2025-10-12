0d593228bc1ffa81e885bd012d5408a2a808231f
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-10-12 16:27:06 -0700

    0001-TWO-SUM Stats: Time: 3 ms (49.92%), Space: 19 MB (23.81%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -3,7 +3,10 @@ class Solution:
         hashM = {}
 
         for i in range(len(nums)):
-            if(target- nums[i] in hashM):
-                return [hashM[target-nums[i]], i]
+            if((target - nums[i]) in hashM):
+                idx = hashM[target - nums[i]]
+                return [idx, i]
             else:
-                hashM[nums[i]]=i
+                hashM[nums[i]] = i
+            # print(hashM)
+        return -1
