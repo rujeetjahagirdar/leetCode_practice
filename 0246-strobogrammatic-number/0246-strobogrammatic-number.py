9c2b9c97a8a065b3e7359ab3bb25723ab4b493bb
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-10-12 17:33:54 -0700

    0246-STROBOGRAMMATIC-NUMBER Stats: Time: 0 ms (100%), Space: 17.7 MB (80.58%) - LeetHub

--- 0246-strobogrammatic-number/0246-strobogrammatic-number.py
+++ 0246-strobogrammatic-number/0246-strobogrammatic-number.py
@@ -1,16 +1,14 @@
 class Solution:
     def isStrobogrammatic(self, num: str) -> bool:
-        
-        mapping = {'6':'9', '9':'6', '8':'8', '1':'1', '0':'0'}
+        mapping = {'1':'1', '6':'9', '8':'8', '0':'0', '9':'6'}
 
         l=0
         r=len(num)-1
 
         while(l<=r):
-            if(num[l] not in mapping or num[r] not in mapping):
-                return False
-            if(num[l]!=mapping[num[r]]):
+            if(num[l] not in mapping or mapping[num[l]]!=num[r]):
                 return False
             l+=1
             r-=1
+
         return True
