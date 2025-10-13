943f8f677082ffb4e4665e52fa2c7761b3f389a8
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-10-13 12:08:24 -0700

    0560-SUBARRAY-SUM-EQUALS-K Stats: Time: 24 ms (90.94%), Space: 20.4 MB (38.87%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,17 +1,22 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         
-        count = {0: 1}
+        #sum of subarray [i...j] would be (sum of subarry [0....j] - sum of subarray [0....i])
+        #so we can use prefix sum to solve this problem
 
-        cumm_sum = 0
+        #we will find sum of subarrays for all j [0...j] and maintain it in hashmap with their count
+        #and check sum of subarray - k in this hashmap and increment ans
+
+        hashM = {0: 1}
+        sum_till_j = 0
         ans=0
 
-        for i in nums:
-            cumm_sum +=i
+        for j in range(len(nums)):
+            sum_till_j += nums[j]
 
-            if(cumm_sum-k in count):
-                ans+=count[cumm_sum-k]
-
-            count[cumm_sum] = count.get(cumm_sum, 0)+1
+            if((sum_till_j - k) in hashM):
+                ans+=hashM[(sum_till_j - k)]
+            
+            hashM[sum_till_j] = hashM.get(sum_till_j, 0) + 1
         
-        return(ans)
+        return ans
