f62a961bc68cd2cf4bcf635b0093af1825762879
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-10-14 15:32:58 -0700

    0807-CUSTOM-SORT-STRING Stats: Time: 1 ms (21.8%), Space: 17.5 MB (98.1%) - LeetHub

--- 0807-custom-sort-string/0807-custom-sort-string.py
+++ 0807-custom-sort-string/0807-custom-sort-string.py
@@ -1,16 +1,24 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
+        
+        #count freq of each char in s
+        #form a string according to order
+
+        #TC: O(length of s)
+        #SC: O(length of s)
+
         c = Counter(s)
+        #{'a':1, 'b':1, 'c':1, 'd':1}
         ans=''
 
         for i in order:
             if(i in c):
                 ans+=i*c[i]
-
-                del c[i]
+                c[i]=0
         
-        if(len(c)>0):
-            for i in c:
+        for i in c:
+            if(c[i]!=0):
                 ans+=i*c[i]
+                c[i]=0
         
-        return(ans)
+        return ans
