db19f9d2072cf5421b9241c5235b78b03006146a
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-10-13 18:25:57 -0700

    0912-RANDOM-PICK-WITH-WEIGHT Stats: Time: 98 ms (27.75%), Space: 23.6 MB (5.36%) - LeetHub

--- 0912-random-pick-with-weight/0912-random-pick-with-weight.py
+++ 0912-random-pick-with-weight/0912-random-pick-with-weight.py
@@ -10,10 +10,22 @@ class Solution:
     def pickIndex(self) -> int:
         randNum = random.randint(1, self.prefix[-1])
 
-        for i in range(len(self.prefix)):
-            if(self.prefix[i]>=randNum):
-                return i
+        # for i in range(len(self.prefix)):
+        #     if(self.prefix[i]>=randNum):
+        #         return i
+        
+        l=0
+        r = len(self.prefix)-1
 
+        while(l<r):
+            mid = (l+r)//2
+            # ans=-1
+            if(self.prefix[mid]>=randNum):
+                # ans = mid
+                r = mid
+            else:
+                l=mid+1
+        return l
 
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(w)
