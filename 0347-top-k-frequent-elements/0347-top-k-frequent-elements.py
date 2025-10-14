c1bbd3f0b9d15fcc47742c62f8adb805290f5fb0
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-10-14 13:20:19 -0700

    0347-TOP-K-FREQUENT-ELEMENTS Stats: Time: 0 ms (100%), Space: 21.4 MB (40%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,8 +1,29 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
+        #TC: O(n logk)
+        #SC: O(n)
+
+        #freq counter
+        #min_heap
+
+        # nums = [1,1,1,2,2,3], k = 2
+
         c = Counter(nums)
+
         #{1: 3, 2:2, 3:1}, k=2
+        #[(3, 1)]
+        #[(3, 1), (2, 2)]
+        #[(2, 2), (3, 1)]
+        #[(2, 2), (3, 1)]
+
+        #Example 2
+        #{1: 3, 2:2, 3:3}, k=2
+        #[(3, 1)]
+        #[(3, 1), (2, 2)]
+        #[(2, 2), (3, 1)]
+        #[(3, 1)]
+        #[(3, 1), (3, 3)]
 
         min_heap = []
 
@@ -15,9 +36,10 @@ class Solution:
                 else:
                     heapq.heappop(min_heap)
                     heapq.heappush(min_heap, (c[i], i))
-            print(min_heap)
+        
         ans=[]
 
-        while min_heap:
-            ans.append(heapq.heappop(min_heap)[1])
+        for i in min_heap:
+            ans.append(i[1])
+        
         return ans
