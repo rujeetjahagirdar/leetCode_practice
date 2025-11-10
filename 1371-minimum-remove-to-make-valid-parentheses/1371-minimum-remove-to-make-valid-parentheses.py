690212e7002ad9b0c96a485784b3f12923c92f86
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-11-10 10:26:31 -0800

    1371-MINIMUM-REMOVE-TO-MAKE-VALID-PARENTHESES Stats: Time: 627 ms (5.15%), Space: 19.3 MB (52.55%) - LeetHub

--- 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.py
@@ -3,21 +3,19 @@ class Solution:
         
         removeIndex = []
 
-        #if opening add to stack
-        #if closing, if stack -1 is opening, pop stack
-                    # else add to stack
-        #make new string by removeing stack index
-
         for i in range(len(s)):
             if(s[i]=='('):
                 removeIndex.append(i)
             elif(s[i]==')'):
-                if(removeIndex and s[removeIndex[-1]]=='('):
-                    removeIndex.pop()
-                else:
+                if(len(removeIndex)==0):
                     removeIndex.append(i)
-        ans=''
-
+                else:
+                    if(s[removeIndex[-1]]=='('):
+                        removeIndex.pop()
+                    else:
+                        removeIndex.append(i)
+        # print(removeIndex)
+        ans = ''
         for i in range(len(s)):
             if(i not in removeIndex):
                 ans+=s[i]
