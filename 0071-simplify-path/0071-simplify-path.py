a9403952d5bd939d46f31db3ab03f442b623f639
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-11-10 10:42:11 -0800

    0071-SIMPLIFY-PATH Stats: Time: 1 ms (48.97%), Space: 18 MB (34.54%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -1,20 +1,19 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        ans=[]
-
+        
         folders = path.split('/')
 
+        ans = []
+
         for f in folders:
-            if(f==''):
+            if(f=='.' or f=='/' or f==''):
                 pass
             elif(f=='..'):
                 if(ans):
                     ans.pop()
-            elif(f=='.'):
-                pass
             else:
                 ans.append(f)
         
         print(ans)
 
-        return "/"+"/".join(ans)
+        return '/'+'/'.join(ans)
