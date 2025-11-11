f0e50b13e71df673acb6c0605f9fbfb7c1412425
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-11-11 13:55:28 -0800

    0347-TOP-K-FREQUENT-ELEMENTS Stats: Time: 7 ms (50.21%), Space: 21.4 MB (39.19%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,43 +1,23 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
-        #TC: O(n logk)
-        #SC: O(n)
-
-        #freq counter
-        #min_heap
-
-        # nums = [1,1,1,2,2,3], k = 2
-
-        c = Counter(nums)
-
-        #{1: 3, 2:2, 3:1}, k=2
-        #[(3, 1)]
-        #[(3, 1), (2, 2)]
-        #[(2, 2), (3, 1)]
-        #[(2, 2), (3, 1)]
-
-        #Example 2
-        #{1: 3, 2:2, 3:3}, k=2
-        #[(3, 1)]
-        #[(3, 1), (2, 2)]
-        #[(2, 2), (3, 1)]
-        #[(3, 1)]
-        #[(3, 1), (3, 3)]
+        #counter with min heap
 
         min_heap = []
-
-        for i in c:
-            if(len(min_heap)<k):
-                heapq.heappush(min_heap, (c[i], i))
-            else:
-                if(c[i]<min_heap[0][0]):
-                    continue
-                else:
-                    heapq.heappop(min_heap)
-                    heapq.heappush(min_heap, (c[i], i))
-        
         ans=[]
+
+        c = Counter(nums)
+        # print(c)
+
+        for n in c:
+            if(len(min_heap)<k):
+                heapq.heappush(min_heap, (c[n], n))
+            else:
+                if(c[n]>min_heap[0][0]):
+                    heapq.heappop(min_heap)
+                    heapq.heappush(min_heap, (c[n], n))
+        
+        # print(min_heap)
 
         for i in min_heap:
             ans.append(i[1])
