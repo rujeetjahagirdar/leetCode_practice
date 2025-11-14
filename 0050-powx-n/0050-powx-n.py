f3ce21cae24699b8d82ebcc294f8ab042728ba18
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-11-14 14:01:06 -0800

    0050-POWX-N Stats: Time: 0 ms (100%), Space: 17.7 MB (94.26%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,18 +1,17 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        
         ans=1
 
         if(n<0):
-            n = -1*n
-            x = (1.0/x)
-        
-        while(n!=0):
-            if(n%2==1):
-                ans*=x
-                n-=1
-            else:
-                x*=x
+            x = (1/x)
+            n= -1 * n
+
+        while(n>0):
+            if(n%2!=0): #n is odd
+                ans= ans * x
+                n=n-1
+            else:       #n is even
+                x = x*x
                 n=n//2
-        print(ans)
-        return ans
+        
+        return(ans)
