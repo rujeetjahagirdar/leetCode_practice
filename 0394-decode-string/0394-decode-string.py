fd8e987751f9d1a139681481aa74711b596872d2
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-11-14 12:57:00 -0800

    0394-DECODE-STRING Stats: Time: 1 ms (4.92%), Space: 17.7 MB (54.3%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,29 +1,70 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        numStack = []
-        strStack = []
-        tStr = ''
-        num = 0
 
-        for i in s:
-            if(i.isnumeric()):
-                num = num*10+int(i)
-            elif(i=='['):
-                # if(tStr):
-                strStack.append(tStr)
-                tStr = ''
-                # if(num>0):
-                numStack.append(num)
-                num=0
-            elif(i==']'):
-                freq = numStack.pop()
-                if(strStack):
-                    tStr = strStack.pop() + freq*tStr
-                else:
-                    tStr = freq*tStr
+        #if opening bracket, put aside curret number and current string
+
+        # numStack = []
+        # strStack = []
+        # tStr = ''
+        # num = 0
+
+        # for i in s:
+        #     if(i.isnumeric()):
+        #         num = num*10+int(i)
+        #     elif(i=='['):
+        #         # if(tStr):
+        #         strStack.append(tStr)
+        #         tStr = ''
+        #         # if(num>0):
+        #         numStack.append(num)
+        #         num=0
+        #     elif(i==']'):
+        #         freq = numStack.pop()
+        #         if(strStack):
+        #             tStr = strStack.pop() + freq*tStr
+        #         else:
+        #             tStr = freq*tStr
+        #     else:
+        #         tStr+=i
+        #     # print(tStr)
+        # return tStr
+
+        #############################
+
+        stack = []
+
+        i=0
+        n=0
+        while(i<len(s)):
+
+            #check digit
+            if(s[i].isnumeric()):
+                # while(s[i].isnumeric()):
+                n = n*10+int(s[i])
+                    # i+=1
             else:
-                tStr+=i
-            # print(tStr)
-        return tStr
+                if(s[i]=='['):
+                    stack.append(n)
+                    n=0
 
-                
+                if(s[i]=='[' or s[i].isalpha()):
+                    stack.append(s[i])
+                else:
+                    tempStr=''
+                    while(stack[-1]!='['):
+                        tempStr+=stack.pop()
+                    tempStr = tempStr[::-1] #reverse the string
+                    stack.pop() #ignore opening bracket
+                    freq = stack.pop() #get freq
+                    tempStr = tempStr*freq
+
+                    for c in tempStr:
+                        stack.append(c)
+            
+            # print(stack)
+            i+=1
+        print(stack)
+        return ''.join(stack)
+                    
+
+            
