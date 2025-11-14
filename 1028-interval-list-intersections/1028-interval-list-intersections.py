68a4ba93844d5f232400668b4b04eb8cecbd0648
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-11-14 12:15:26 -0800

    1028-INTERVAL-LIST-INTERSECTIONS Stats: Time: 11 ms (16.5%), Space: 18.5 MB (68.74%) - LeetHub

--- 1028-interval-list-intersections/1028-interval-list-intersections.py
+++ 1028-interval-list-intersections/1028-interval-list-intersections.py
@@ -8,15 +8,18 @@ class Solution:
         i=j=0
 
         while(i<len(firstList) and j<len(secondList)):
-            if(firstList[i][0]>secondList[j][1]):
-                j+=1
-                continue
-            if(secondList[j][0]>firstList[i][1]):
-                i+=1
-                continue
+            # if(firstList[i][0]>secondList[j][1]):
+            #     j+=1
+            #     continue
+            # if(secondList[j][0]>firstList[i][1]):
+            #     i+=1
+            #     continue
+
             aStart = max(firstList[i][0], secondList[j][0])
             aEnd = min(firstList[i][1], secondList[j][1])
-            ans.append([aStart, aEnd])
+
+            if(aStart<=aEnd):
+                ans.append([aStart, aEnd])
 
             if(firstList[i][1]<secondList[j][1]):
                 i+=1
