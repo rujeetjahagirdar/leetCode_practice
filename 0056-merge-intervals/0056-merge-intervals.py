74e808741ac635d15a3fff73a1cc7d48390d3497
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-11-14 14:48:51 -0800

    0056-MERGE-INTERVALS Stats: Time: 3 ms (93.02%), Space: 21.7 MB (16.77%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,22 +1,17 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         
-        #first sort input based on first value (start time)
-        #then use stack and append intervals my merging with stack -1
+        #stack
+        intervals = sorted(intervals, key=lambda x: x[0])
 
-        #TC : O(n logn)
-        #SP : O(n)
 
-        sorted_intervals = sorted(intervals, key= lambda x: x[0])
+        stack = [intervals[0]]
 
-        stack = [sorted_intervals[0]]
+        for i in range(1, len(intervals)):
+            s, e = intervals[i]
 
-        for i in range(1, len(sorted_intervals)):
-            s, e = sorted_intervals[i]
-
-            if(stack[-1][0]<=s<=stack[-1][1]):
-                stack[-1][1] = max(stack[-1][1], e)
-            
+            if(s<=stack[-1][1]): #overlap
+                stack[-1][1] = max(e, stack[-1][1])
             else:
                 stack.append([s, e])
         
