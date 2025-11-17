72c43ebeccc703b5e47d95f32ab063df5ff9a390
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-11-17 14:52:18 -0800

    1909-BUILDINGS-WITH-AN-OCEAN-VIEW Stats: Time: 16 ms (72.73%), Space: 31.1 MB (33.6%) - LeetHub

--- 1909-buildings-with-an-ocean-view/1909-buildings-with-an-ocean-view.py
+++ 1909-buildings-with-an-ocean-view/1909-buildings-with-an-ocean-view.py
@@ -1,12 +1,16 @@
 class Solution:
     def findBuildings(self, heights: List[int]) -> List[int]:
+
+        #TC: O(n)
+        #SC: O(n)
+
         ans=[]
 
-        mxTillNow = 0
+        mxTill = 0
 
         for i in range(len(heights)-1, -1, -1):
-            if(heights[i]>mxTillNow):
+            if(heights[i]>mxTill):
                 ans.append(i)
-            mxTillNow = max(mxTillNow, heights[i])
-
+                mxTill = heights[i]
+        
         return ans[::-1]
