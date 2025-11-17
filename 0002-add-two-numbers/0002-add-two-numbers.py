ab40ff23f226a864025b40361fba3b1fc2f11ae2
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-11-17 14:31:48 -0800

    0002-ADD-TWO-NUMBERS Stats: Time: 3 ms (72.99%), Space: 17.7 MB (84.63%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,45 +5,49 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        #TC: O(max(m, n))
+        #SC: O(1)
         
-        ans= dummy = ListNode()
+        carry = 0
+        head = curr = ListNode(-1)
 
-        if not l1:
+        if(not l1):
             return l2
         
-        if not l2:
+        if(not l2):
             return l1
 
-        carry=0
-        
         while(l1 or l2):
-
             if(l1):
                 n1 = l1.val
             else:
                 n1 = 0
+
             if(l2):
                 n2 = l2.val
             else:
-                n2 = 0
+                n2=0
             
-            sm = n1+n2+carry
-            
-            
+            sm = (n1+n2)+carry
+
+            curr.next = (ListNode(sm%10))
+
             carry = (sm)//10
-            dummy.next = ListNode((sm)%10)
-            
+
             if(l1):
-                l1=l1.next
+                l1 = l1.next
             else:
                 l1=None
+            
             if(l2):
-                l2=l2.next
+                l2 = l2.next
             else:
-                l2=None
-            dummy = dummy.next
+                l2 = None
+            
+            curr = curr.next
         
         if(carry>0):
-            dummy.next = ListNode(carry)
-        print(ans)
-        return ans.next
+            curr.next = ListNode(carry)
+            carry = 0
+
+        return head.next
