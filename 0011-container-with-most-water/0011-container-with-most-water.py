bcd1a1a618d7d90b35c7fef0a1416c395c15c6a3
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-11-17 18:34:52 -0800

    0011-CONTAINER-WITH-MOST-WATER Stats: Time: 91 ms (78.91%), Space: 28.6 MB (23.94%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,17 +1,20 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
+        
+        #two pointer
+
         ans=0
 
         l=0
         r = len(height)-1
 
         while(l<r):
-            area = min(height[l], height[r]) * (r-l)
+            area = min(height[l], height[r])* (r-l)
             ans = max(ans, area)
 
-            if(height[l]<height[r]):
+            if(height[l]<=height[r]):
                 l+=1
             else:
                 r-=1
         
-        return ans
+        return(ans)
