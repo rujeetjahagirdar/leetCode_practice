73c4fd7638204938468d6ac50fb116e4f770e1a1
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-11-17 18:31:15 -0800

    1320-REMOVE-ALL-ADJACENT-DUPLICATES-IN-STRING-II Stats: Time: 63 ms (17.24%), Space: 21.8 MB (76.61%) - LeetHub

--- 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.py
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.py
@@ -1,26 +1,32 @@
 class Solution:
     def removeDuplicates(self, s: str, k: int) -> str:
-        
-        #(lastChar, freq of lastChar)
-        stack = []
 
         
+        
+        stack = [] #(lastChar, freq of lastChar)
+
+        if(not s):
+            return ''
+        
+        # stack.append((s[0], 1))
 
         for i in range(len(s)):
-
-            if(stack and s[i]==stack[-1][0]):
-                char, freq = stack.pop()
-                stack.append((char, freq+1))
+            if(stack):
+                if(s[i]==stack[-1][0]):
+                    stack[-1] = (stack[-1][0], stack[-1][1]+1)
+                else:
+                    stack.append((s[i], 1))
             else:
                 stack.append((s[i], 1))
             
-            if(stack[-1][1]==k):
+            if(stack[-1][1]>=k):
                 stack.pop()
-            # print(stack)
         
+        print(stack)
+
         ans=''
 
-        for i in stack:
-            ans+=i[0]*i[1]
-        # print(ans)
-        return ans
+        for c, f in stack:
+            ans+=c*f
+        
+        return(ans)
