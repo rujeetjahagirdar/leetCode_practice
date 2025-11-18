16e9543e06566f2d8a0b99fa51e4d4e8ecad48f9
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-11-17 19:23:51 -0800

    0008-STRING-TO-INTEGER-ATOI Stats: Time: 0 ms (100%), Space: 17.8 MB (48.97%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,35 +1,24 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        sign = []
-        number = []
-        s=s.strip()
-        for i in range(len(s)):
-            if(s[i]==" "):
-                break
-            elif(s[i]=="-" or s[i]=="+"):
-                if(i==0 or s[i-1]==' '):
-                    sign.append(s[i])
-                else:
-                    break
-            elif(s[i].isnumeric()):
-                number.append(s[i])
-            else:
-                break
-        print(sign)
-        print(number)
-        ans=''
-        if(len(number)!=0):
-            for i in range(len(number)):
-                if(len(number)==1 and number[i]=='0'):
-                    ans =0
-                else:
-                    ans +=number[i]
-        else:
-            return 0
-        if(len(sign)!=0):
-            if(sign[0]=='-'):
-                ans = -1 * int(ans)
-            else:
-                ans = int(ans)
-        print(int(ans))
-        return max(-2**31, min(int(ans), 2**31 - 1))
+        sign=None
+        n=0
+
+        i=0
+        while(i<len(s) and s[i]==' '):
+            i+=1
+        
+        if(i<len(s) and s[i]=='-'):
+            sign='-'
+            i+=1
+        elif(i<len(s) and s[i]=='+'):
+            sign='+'
+            i+=1
+        while(i<len(s) and s[i].isnumeric()):
+            n = n*10+int(s[i])
+            i+=1
+        
+        if(sign=='-'):
+            n = -1*n
+            return max(-2**31, n)
+        
+        return min(n, 2**31 -1)
