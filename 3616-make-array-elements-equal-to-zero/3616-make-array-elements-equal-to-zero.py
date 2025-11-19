89fbdb8884a9f74797b4da0d1b98e83de4ccfb80
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-11-19 12:35:40 -0800

    3616-MAKE-ARRAY-ELEMENTS-EQUAL-TO-ZERO Stats: Time: 50 ms (49.92%), Space: 17.9 MB (9.98%) - LeetHub

--- 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.py
+++ 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.py
@@ -1,31 +1,51 @@
 class Solution:
     def countValidSelections(self, nums: List[int]) -> int:
-        
-        leftSum = [0]*len(nums)
-        rightSum = [0]*len(nums)
 
+        totalSum = sum(nums)
+        prefixSum = [-1]*len(nums) #excluding self
         cSum=0
         for i in range(len(nums)):
-            leftSum[i]=cSum
+            prefixSum[i]=cSum
             cSum+=nums[i]
-
-        print(leftSum)
-
-        cSum=0
-        for i in range(len(nums)-1, -1, -1):
-            rightSum[i]=cSum
-            cSum+=nums[i]
-        print(rightSum)
-
-        ans = []
-
-        for i in range(len(leftSum)):
-            if(nums[i]==0):
-                if(leftSum[i]==rightSum[i]):
-                    ans.append(i)
-                    ans.append(i)
-                elif(abs(leftSum[i]-rightSum[i])==1):
-                    ans.append(i)
         
-        print(ans)
-        return len(ans)
+        ans=0
+
+        for i in range(len(nums)):
+            if(nums[i]==0):
+                rightSum = totalSum - prefixSum[i]
+                if(prefixSum[i]==rightSum):
+                    ans+=2*1
+                elif(abs(rightSum-prefixSum[i])==1):
+                    ans+=1
+        
+        return ans
+
+        ###################################
+        # leftSum = [0]*len(nums)
+        # rightSum = [0]*len(nums)
+
+        # cSum=0
+        # for i in range(len(nums)):
+        #     leftSum[i]=cSum
+        #     cSum+=nums[i]
+
+        # print(leftSum)
+
+        # cSum=0
+        # for i in range(len(nums)-1, -1, -1):
+        #     rightSum[i]=cSum
+        #     cSum+=nums[i]
+        # print(rightSum)
+
+        # ans = []
+
+        # for i in range(len(leftSum)):
+        #     if(nums[i]==0):
+        #         if(leftSum[i]==rightSum[i]):
+        #             ans.append(i)
+        #             ans.append(i)
+        #         elif(abs(leftSum[i]-rightSum[i])==1):
+        #             ans.append(i)
+        
+        # print(ans)
+        # return len(ans)
