28c91081e46c8a4ab0bcf40a6a7738239514a103
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-11-22 18:51:46 -0800

    1171-SHORTEST-PATH-IN-BINARY-MATRIX Stats: Time: 239 ms (30.65%), Space: 19.6 MB (17.78%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
@@ -1,30 +1,33 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         
-        #dfs
-
-        if(grid[0][0]==1 or grid[-1][-1]==1):
-            return -1
-        
-        directions = [(0,1), (0,-1), (1,0), (1,1), (-1, 0), (-1, -1), (1, -1), (-1, 1)]
-        visited = set()
-        self.ans=0
+        #bfs
 
         q = deque()
+        visited = set()
+
+        directoins = [(0, -1), (1, 0), (0, 1), (-1, 0), (1, 1), (-1, 1), (1, -1), (-1, -1)]
+
+        if(grid[0][0]!=0 or grid[-1][-1]!=0):
+            return -1
+        
         q.append((0, 0, 1))
+        visited.add((0, 0))
+
+
 
         while q:
-            i, j, path_length = q.popleft()
+            for _ in range(len(q)):
+                i, j, path_length = q.popleft()
 
-            if(i==len(grid)-1 and j==len(grid[0])-1):
-                return path_length
-            
-            for direction in directions:
-                newi, newj = i+direction[0], j+direction[1]
+                if(i==len(grid)-1 and j==len(grid[0])-1):
+                    return path_length
+                
+                for direction in directoins:
+                    newi, newj = i+direction[0], j+direction[1]
 
-                if(0<=newi<=len(grid)-1 and 0<=newj<=len(grid[0])-1 and (newi, newj) not in visited and grid[newi][newj]==0):
-                    q.append((newi, newj, path_length+1))
-                    visited.add((newi, newj))        
+                    if(0<=newi<len(grid) and 0<=newj<len(grid[0]) and (newi, newj) not in visited and grid[newi][newj]==0):
+                        q.append((newi, newj, path_length+1))
+                        visited.add((newi, newj))
         
-
         return -1
