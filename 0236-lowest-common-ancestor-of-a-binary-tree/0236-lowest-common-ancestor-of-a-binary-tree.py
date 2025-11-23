d3092a84e44f0701f7d0dd6a29f42ade9a4945f3
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-11-22 17:53:09 -0800

    0236-LOWEST-COMMON-ANCESTOR-OF-A-BINARY-TREE Stats: Time: 53 ms (62.54%), Space: 22.1 MB (43.14%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -8,6 +8,12 @@
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         
+        #dfs, bottom up
+        #process current node after processing children
+
+        #TC: O(n)
+        #SC: O(n)
+
         def dfs(node):
             if not node:
                 return None
@@ -15,14 +21,12 @@ class Solution:
             if(node==p or node==q):
                 return node
             
-            left = dfs(node.left)
-            right = dfs(node.right)
+            left_found = dfs(node.left)
+            right_found = dfs(node.right)
 
-            if left and right:
+            if(left_found and right_found):
                 return node
-            
-            return left or right
 
+            return left_found or right_found
         
-
-        return dfs(root)
+        return(dfs(root))
