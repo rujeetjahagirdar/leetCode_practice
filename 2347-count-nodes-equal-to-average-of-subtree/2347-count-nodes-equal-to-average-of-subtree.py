fee9f1aa2c1bce10064ae5c42cd4ac888bc61bf8
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-11-23 17:40:22 -0800

    2347-COUNT-NODES-EQUAL-TO-AVERAGE-OF-SUBTREE Stats: Time: 49 ms (31.1%), Space: 18.2 MB (27.06%) - LeetHub

--- 2347-count-nodes-equal-to-average-of-subtree/2347-count-nodes-equal-to-average-of-subtree.py
+++ 2347-count-nodes-equal-to-average-of-subtree/2347-count-nodes-equal-to-average-of-subtree.py
@@ -7,27 +7,28 @@
 class Solution:
     def averageOfSubtree(self, root: TreeNode) -> int:
         
-        #dfs, bottom up
-        #process current node after processing children
-
+        #dfs, bottom-up
         self.ans=0
 
         def dfs(node):
             if not node:
-                return (0, 0)
-            
+                return (0, 0) #(sum, count)
+
             left_sum, left_count = dfs(node.left)
             right_sum, right_count = dfs(node.right)
 
-            subtreeSum = (left_sum+right_sum+node.val)
-            subtreeCount = (left_count+right_count+1)
-            
-            subtreeAvg = int(subtreeSum/subtreeCount)
+            subtreeSum = left_sum+right_sum+node.val
+            subtreeCount = left_count+right_count+1
+
+            subtreeAvg = subtreeSum // subtreeCount
 
             if(subtreeAvg==node.val):
                 self.ans+=1
             
             return (subtreeSum, subtreeCount)
+
+        if not root:
+            return 0
         
         dfs(root)
 
