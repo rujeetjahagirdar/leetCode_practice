7998e4d339165893df24f0b483f01abffc45a315
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-11-25 19:16:51 -0800

    0065-VALID-NUMBER Stats: Time: 0 ms (100%), Space: 17.9 MB (19.89%) - LeetHub

--- 0065-valid-number/0065-valid-number.py
+++ 0065-valid-number/0065-valid-number.py
@@ -1,4 +1,35 @@
 class Solution:
     def isNumber(self, s: str) -> bool:
-        pattern = r"^[+-]?(\d+\.?\d*|\.\d+)([eE][+-]?\d+)?$"
-        return(bool(re.match(pattern,s)))
+        
+        seenDigit = False
+        seenDot = False
+        seenExponent = False
+        seenSign = False
+
+        for i in range(len(s)):
+            if(s[i].isdigit()):
+                seenDigit = True
+            elif(s[i] in ['+','-']):
+                #sign should be either at first or immediately after exponent
+                if(i>0 and s[i-1] not in ('e','E')):
+                    return False
+                seenSign=True
+            elif(s[i] in ('e','E')):
+                #exponent must come after digit
+                #must have only one exponent
+                if(not seenDigit or seenExponent):
+                    return False
+                seenExponent = True
+                seenDigit = False
+            elif(s[i]=='.'):
+                #only one dot
+                #can not have dot after anytime exponent 
+                if(seenDot or seenExponent):
+                    return False
+                seenDot = True
+            
+            else:
+                #everything else, return False
+                return False
+        
+        return seenDigit
