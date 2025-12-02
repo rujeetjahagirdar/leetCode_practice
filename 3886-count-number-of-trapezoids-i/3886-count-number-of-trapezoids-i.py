7105ec893e24350bc0c3a07df012c95a9efda9e7
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-12-02 13:28:41 -0800

    3886-COUNT-NUMBER-OF-TRAPEZOIDS-I Stats: Time: 111 ms (5.97%), Space: 63.6 MB (42.9%) - LeetHub

--- 3886-count-number-of-trapezoids-i/3886-count-number-of-trapezoids-i.py
+++ 3886-count-number-of-trapezoids-i/3886-count-number-of-trapezoids-i.py
@@ -1,39 +1,31 @@
 class Solution:
     def countTrapezoids(self, points: List[List[int]]) -> int:
         
-        points_be_height = defaultdict(int)
+        #combination of every edge on a particular y level with every other edges on different y levels
 
+        #count x points at every y level
+        points_by_height = defaultdict(int)
         for x, y in points:
-            points_be_height[y]+=1
+            points_by_height[y]+=1
         
-        # print(points_be_height)
-
+        #number of edges that can be formed at each y level
         edges_by_height = defaultdict(int)
-
-        for height in points_be_height:
-            n = points_be_height[height]
+        for height in points_by_height:
+            n = points_by_height[height]
             r = 2
             if(n>=2):
-                # edges_by_height[height] = math.factorial(n) / (math.factorial(n-r) * math.factorial(r))  #nCr
+                #nCr
                 edges_by_height[height] = math.comb(n, r)
-            # else:
-            #     edges_by_height[height] = 0
         
         # print("edges: ", edges_by_height)
 
         if(not edges_by_height):
             return 0
 
+
         ans=0
         edges_by_height_val = list(edges_by_height.values())
         mod = 10**9 + 7
-        
-        # for i in range(len(edges_by_height_val)-1):
-        #     for j in range(i+1, len(edges_by_height_val)):
-        #         ans+=edges_by_height_val[i]*edges_by_height_val[j]
-        
-        # print(ans%mod)
-        # return int(ans%mod) 
 
         prefixSumRight = [-1]*len(edges_by_height_val)
         cummSum = 0
