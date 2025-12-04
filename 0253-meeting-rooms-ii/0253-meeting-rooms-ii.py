1ed191a7f6b22ef60649e4d7052f5ffa572e30ed
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2025-12-04 15:37:39 -0800

    0253-MEETING-ROOMS-II Stats: Time: 15 ms (5.29%), Space: 20.3 MB (17.48%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -10,16 +10,31 @@ class Solution:
             #while its less than current start, pop from min_heap
         #guess, ans will be len of min_heap
         print(intervals)
+        # for start, end in intervals:
+        #     if(min_heap and start>=min_heap[0]):
+        #         while(min_heap and min_heap[0]<=start):
+        #             heapq.heappop(min_heap)
+        #             occupiedRooms-=1
+        #         heapq.heappush(min_heap, end)
+        #         occupiedRooms+=1
+        #     else:
+        #         occupiedRooms+=1
+        #         heapq.heappush(min_heap, end)
+        #     ans=max(ans, occupiedRooms)
+        
+        #     print(min_heap)
+        #     print(ans)
+        #     print(occupiedRooms)
+        # return ans
+
+
         for start, end in intervals:
-            if(min_heap and start>=min_heap[0]):
-                while(min_heap and min_heap[0]<=start):
-                    heapq.heappop(min_heap)
-                    occupiedRooms-=1
-                heapq.heappush(min_heap, end)
-                occupiedRooms+=1
-            else:
-                occupiedRooms+=1
-                heapq.heappush(min_heap, end)
+            while(min_heap and min_heap[0]<=start):
+                heapq.heappop(min_heap)
+                occupiedRooms-=1
+            heapq.heappush(min_heap, end)
+            occupiedRooms+=1
+            
             ans=max(ans, occupiedRooms)
         
             print(min_heap)
