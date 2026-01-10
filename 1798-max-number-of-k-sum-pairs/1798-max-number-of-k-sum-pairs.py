e2d4d459a24c1c62806a50e1d16d0e99966bb617
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2026-01-10 14:44:54 +0530

    1798-MAX-NUMBER-OF-K-SUM-PAIRS Stats: Time: 431 ms (99.35%), Space: 32.5 MB (5.87%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.py
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.py
@@ -3,15 +3,18 @@ class Solution:
         ans=0
         c = Counter(nums) #{3: 3, 1: 1, 4:1}, k=6
         
-        for i in c:
+        for i in list(c.keys()):
             if(k-i in c):
                 if((k-i)==i):
                     ans+=c[i]//2
-                    c[i]=0
+                    # c[i]=0
+                    del c[i]
                 else:
                     ans+=min(c[i], c[k-i])
-                    c[i]=0
-                    c[k-i]=0
+                    # c[i]=0
+                    # c[k-i]=0
+                    del c[i]
+                    del c[k-i]
         print(ans)
         return ans
         
