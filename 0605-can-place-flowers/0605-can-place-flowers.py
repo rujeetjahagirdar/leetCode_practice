db876d618027e843fd92360c2e96da09850ed629
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2026-01-10 09:13:25 +0530

    0605-CAN-PLACE-FLOWERS Stats: Time: 7 ms (74.54%), Space: 19.5 MB (5.12%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,8 +1,8 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
         
-        if(n==0):
-            return True
+        # if(n==0):
+        #     return True
 
         for i in range(len(flowerbed)):
             if(flowerbed[i]==0):
@@ -19,6 +19,6 @@ class Solution:
                     flowerbed[i]=1
                     n-=1
                 
-                if(n==0):
+                if(n<=0):
                     return True
         return n==0
