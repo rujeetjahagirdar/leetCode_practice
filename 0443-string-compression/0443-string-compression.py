ec9fd50fbbddf2b5d0e57ba93e391532db9341d6
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2026-01-10 09:57:03 +0530

    0443-STRING-COMPRESSION Stats: Time: 4 ms (13.96%), Space: 19.6 MB (5.1%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -2,45 +2,22 @@ class Solution:
     def compress(self, chars: List[str]) -> int:
         
         i=j=0
-        # freq=1
-        singles=0
-        groups=0
-        while(i<len(chars)):
+        count=0
 
-            char = chars[i]
-            # j=i+1
-            count=0
-            while(i<len(chars) and chars[i]==char):
-                i+=1
-                count+=1
-            
-            chars[j]=char
+        while(j<len(chars)):
+            c = chars[j]
+            count=1
             j+=1
 
+            while(j<len(chars) and chars[j]==c):
+                count+=1
+                j+=1
+            
+            chars[i]=c
+            i+=1
+
             if(count>1):
-                for digit in str(count):
-                    chars[j]=digit
-                    j+=1
-            
-            
-            
-
-            # while(i<len(chars) and chars[i]==chars[j]):
-            #     # freq+=1
-            #     i+=1
-            # if(i>j+1):
-            #     groups+=1
-                
-            #     strFreq = str((i-j))
-            #     groups+=len(strFreq)
-            #     chars[j+1: j+1+len(strFreq)] = strFreq
-            #     j=j+1+len(strFreq)
-            # else:
-            #     singles+=1
-            # j=i
-            # i+=1
-        print(chars)
-        print(singles)
-        print(groups)
-
-        return j
+                for x in str(count):
+                    chars[i]=x
+                    i+=1
+        return i
