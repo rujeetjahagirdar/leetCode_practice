8f8601ecb6d4e42f7740042c88ef82ada06a0d03
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2026-01-14 19:32:38 +0530

    0150-EVALUATE-REVERSE-POLISH-NOTATION Stats: Time: 0 ms (100%), Space: 20.9 MB (8.96%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,28 +1,56 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
+        # if(len(tokens)==1):
+        #     return int(tokens[0])
+        
+        # stack=[]
+
+        # for i in range(len(tokens)):
+        #     if(tokens[i][0]=='-' and tokens[i][1:].isdigit()):
+        #         stack.append(int(tokens[i]))
+        #     elif(tokens[i].isdigit()):
+        #         stack.append(int(tokens[i]))
+        #     else:
+        #         num2 = stack.pop(-1)
+        #         num1 = stack.pop(-1)
+        #         if(tokens[i]=='+'):
+        #             result = num1 + num2
+        #         elif(tokens[i]=='-'):
+        #             result = num1 - num2
+        #         elif(tokens[i]=='*'):
+        #             result = num1 * num2
+        #         elif(tokens[i]=='/'):
+        #             result = int(num1 / num2)
+        #         stack.append(result)
+        #     # print(stack)
+        
+        # # print(result)
+        # return stack[0]
+
         if(len(tokens)==1):
             return int(tokens[0])
         
         stack=[]
 
-        for i in range(len(tokens)):
-            if(tokens[i][0]=='-' and tokens[i][1:].isdigit()):
-                stack.append(int(tokens[i]))
-            elif(tokens[i].isdigit()):
-                stack.append(int(tokens[i]))
+        for token in tokens:
+            if(token.isdigit() or (token[0]=='-' and token[1:].isdigit())):
+                stack.append(int(token))
             else:
-                num2 = stack.pop(-1)
-                num1 = stack.pop(-1)
-                if(tokens[i]=='+'):
-                    result = num1 + num2
-                elif(tokens[i]=='-'):
-                    result = num1 - num2
-                elif(tokens[i]=='*'):
-                    result = num1 * num2
-                elif(tokens[i]=='/'):
-                    result = int(num1 / num2)
-                stack.append(result)
+                if(len(stack)<2):
+                    return False
+                else:
+                    op = token
+                    num2 = stack.pop(-1)
+                    num1 = stack.pop(-1)
+
+                    if(op=='+'):
+                        stack.append(num1+num2)
+                    elif(op=='-'):
+                        stack.append(num1-num2)
+                    elif(op=='/'):
+                        stack.append(int(num1/num2))
+                    elif(op=='*'):
+                        stack.append(num1*num2)
             # print(stack)
         
-        # print(result)
         return stack[0]
