ab3efa4037ddd4e836b76c0e1c50adfb3f2da7be
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2026-01-15 07:45:37 +0530

    0739-DAILY-TEMPERATURES Stats: Time: 116 ms (40.72%), Space: 28.1 MB (41.99%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -4,6 +4,8 @@ class Solution:
 
         stack = [0]
 
+        #for each element in temperatures, all of its previous elements will be poped and only current element is pushed, so effectively every element is pushed only once and poped only once, so it will be O(n)
+
         for j in range(1, len(temperatures)):
             while(stack and temperatures[j]>temperatures[stack[-1]]):
                 i = stack.pop()
@@ -11,3 +13,5 @@ class Solution:
             stack.append(j)
         print(ans)
         return ans
+
+        
