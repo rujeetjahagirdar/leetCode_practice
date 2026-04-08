708d2532a8cff881073b490c882c07b0802daa7c
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2026-04-08 15:02:19 +0530

    0584-FIND-CUSTOMER-REFEREE Stats: Time: 283 ms (61.47%), Space: 67 MB (77.84%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.py
+++ 0584-find-customer-referee/0584-find-customer-referee.py
@@ -1,10 +1,14 @@
 import pandas as pd
 
 def find_customer_referee(customer: pd.DataFrame) -> pd.DataFrame:
-    temp = pd.merge(customer, customer, left_on="referee_id", right_on="id", how="left", suffixes= ("_cust","_ref"))
+    # temp = pd.merge(customer, customer, left_on="referee_id", right_on="id", how="left", suffixes= ("_cust","_ref"))
 
-    print(temp)
+    # print(temp)
 
-    result = temp[(temp["referee_id_cust"].isna()) | (temp['referee_id_cust']!=2)]
+    # result = temp[(temp["referee_id_cust"].isna()) | (temp['referee_id_cust']!=2)]
 
-    return(pd.DataFrame({'name':result['name_cust']}))
+    # return(pd.DataFrame({'name':result['name_cust']}))
+
+    result = customer[(customer['referee_id'].isna()) | (customer['referee_id']!=2)]
+
+    return pd.DataFrame({"name": result['name']})
