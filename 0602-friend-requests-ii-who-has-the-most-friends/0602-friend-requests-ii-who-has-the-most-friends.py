678b9227a8c49003f63e37352ad1cf41c2c3fc45
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2026-04-08 18:52:15 +0530

    0602-FRIEND-REQUESTS-II-WHO-HAS-THE-MOST-FRIENDS Stats: Time: 282 ms (61.89%), Space: 66.9 MB (82.57%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.py
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.py
@@ -1,11 +1,15 @@
 import pandas as pd
 
 def most_friends(request_accepted: pd.DataFrame) -> pd.DataFrame:
-    req_counts = request_accepted["requester_id"].value_counts()
-    accep_counts = request_accepted["accepter_id"].value_counts()
+    # req_counts = request_accepted["requester_id"].value_counts()
+    # accep_counts = request_accepted["accepter_id"].value_counts()
 
-    total_counts = req_counts.add(accep_counts, fill_value=0)
+    # total_counts = req_counts.add(accep_counts, fill_value=0)
 
-    print(total_counts)
+    # print(total_counts)
 
-    return pd.DataFrame({"id": [total_counts.idxmax()], "num": [total_counts.max()]})
+    # return pd.DataFrame({"id": [total_counts.idxmax()], "num": [total_counts.max()]})
+
+    temp = pd.concat([request_accepted['requester_id'], request_accepted['accepter_id']]).value_counts()
+
+    return pd.DataFrame({"id": [temp.idxmax()], "num": [temp.max()]})
