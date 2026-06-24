cd00cc02a6b76185cecb9d873c963bae4b7ef65b
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2026-06-24 22:24:39 +0530

    0176-SECOND-HIGHEST-SALARY Stats: Time: 269 ms (39.42%), Space: 67.9 MB (5.59%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.py
+++ 0176-second-highest-salary/0176-second-highest-salary.py
@@ -1,12 +1,22 @@
 import pandas as pd
 
 def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
-    unique_salaries = employee['salary'].drop_duplicates()
-    sorted_salaries = unique_salaries.sort_values(ascending=False)
-    # print(sorted_salaries)
-    if(len(sorted_salaries)<2):
-        result = pd.DataFrame({'SecondHighestSalary':[None]})
-    else:
-        result = pd.DataFrame({'SecondHighestSalary':[sorted_salaries.iloc[1]]})
+    # unique_salaries = employee['salary'].drop_duplicates()
+    # sorted_salaries = unique_salaries.sort_values(ascending=False)
+    # # print(sorted_salaries)
+    # if(len(sorted_salaries)<2):
+    #     result = pd.DataFrame({'SecondHighestSalary':[None]})
+    # else:
+    #     result = pd.DataFrame({'SecondHighestSalary':[sorted_salaries.iloc[1]]})
     
-    return(result)
+    # return(result)
+
+    unique_salaries = employee.drop_duplicates(subset=['salary'])
+    unique_salaries = unique_salaries.nlargest(2, columns=['salary'])
+
+    print(unique_salaries)
+
+    if(len(unique_salaries)<2):
+        return pd.DataFrame({"SecondHighestSalary": [None]})
+    else:
+        return pd.DataFrame({"SecondHighestSalary": [unique_salaries.iloc[-1]['salary']]})
