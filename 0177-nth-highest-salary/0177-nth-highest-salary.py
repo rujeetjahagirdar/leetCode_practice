d11a2d4cb550c157a376edf88028c2ee3dc0e3e6
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2026-06-24 22:14:52 +0530

    0177-NTH-HIGHEST-SALARY Stats: Time: 292 ms (23.42%), Space: 67.3 MB (33.56%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.py
+++ 0177-nth-highest-salary/0177-nth-highest-salary.py
@@ -1,11 +1,12 @@
 import pandas as pd
 
 def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
-    unique_salary = employee['salary'].drop_duplicates()
-    # print(type(unique_salary))
-    result = unique_salary.nlargest(N).reset_index(drop=True)
-    # print(result)
-    if(len(result)<N or N<=0):
-        return pd.DataFrame({f'getNthHighestSalary({N})':[None]})
+    unique_salaries = employee.drop_duplicates(subset='salary')
+
+    temp = unique_salaries.nlargest(N, columns=["salary"])
+    print(temp)
+
+    if(len(temp)<N or N<=0):
+        return pd.DataFrame({f"getNthHighestSalary({N})": [None]})
     else:
-        return pd.DataFrame({f'getNthHighestSalary({N})':[result.iloc[-1]]})
+        return pd.DataFrame({f"getNthHighestSalary({N})": [temp.iloc[-1]['salary']]})
