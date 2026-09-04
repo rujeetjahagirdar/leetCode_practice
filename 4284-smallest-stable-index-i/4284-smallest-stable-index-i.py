6ac5e9862a3c6a6ef5575fb2b650a74114ffd7a6
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2026-09-04 10:27:40 +0530

    4284-SMALLEST-STABLE-INDEX-I Stats: Time: 3 ms (77.34%), Space: 19.3 MB (31.28%) - LeetHub

--- 4284-smallest-stable-index-i/4284-smallest-stable-index-i.py
+++ 4284-smallest-stable-index-i/4284-smallest-stable-index-i.py
@@ -1,13 +1,31 @@
 class Solution:
     def firstStableIndex(self, nums: list[int], k: int) -> int:
+        # mxVal = float("-INF")
+        # mnVal = float("INF")
+
+        # for i in range(len(nums)):
+        #     mxVal = max(mxVal, nums[i])
+        #     mnVal = min(nums[i:])
+
+        #     if(mxVal - mnVal <=k):
+        #         return i
+        
+        # return -1
+
+        mins = [-1]*len(nums)
+        mins[-1] = nums[-1]
+
+        for i in reversed(range(len(nums)-1)):
+            mins[i] = min(nums[i], mins[i+1])
+        
+        # print(mins)
+
         mxVal = float("-INF")
-        mnVal = float("INF")
 
         for i in range(len(nums)):
             mxVal = max(mxVal, nums[i])
-            mnVal = min(nums[i:])
 
-            if(mxVal - mnVal <=k):
+            if((mxVal - mins[i]) <=k):
                 return i
         
         return -1
