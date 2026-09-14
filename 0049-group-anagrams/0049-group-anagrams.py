407495baa13a2478c88e79398ce6d48e07c3b322
Rujeet J. <19625813+rujeetjahagirdar@users.noreply.github.com>
2026-09-14 16:20:05 +0530

    0049-GROUP-ANAGRAMS Stats: Time: 31 ms (7.49%), Space: 27.2 MB (5.05%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,20 +1,33 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
-        groups = defaultdict(list)
+        # groups = defaultdict(list)
+
+        # # for s in strs:
+        # #     srtedS = ''.join(sorted(s))
+        # #     groups[srtedS].append(s)
+        
+        # # return list(groups.values())
 
         # for s in strs:
-        #     srtedS = ''.join(sorted(s))
-        #     groups[srtedS].append(s)
+        #     counts = [0]*26
+
+        #     for i in s:
+        #         counts[ord(i)-ord('a')]+=1
+            
+        #     groups[tuple(counts)].append(s)
         
         # return list(groups.values())
 
-        for s in strs:
-            counts = [0]*26
+        groups = defaultdict(list)
 
-            for i in s:
-                counts[ord(i)-ord('a')]+=1
-            
-            groups[tuple(counts)].append(s)
+        for s in strs:
+            freq = [0]*26
+            for c in s:
+                freq[ord(c)-ord('a')]+=1
+            groups[tuple(freq)].append(s)
+            # groups[''.join(sorted(s))].append(s)
         
+        print(groups)
+
         return list(groups.values())
